b2903864600ae6c134033cce93a61c1f634b7a0b
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-05-22 23:07:45 +0200

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         Carbon::setLocale(config('app.locale'));
         Pulse::users(function ($ids) {
             return User::findMany($ids)->map(fn ($user) => [
