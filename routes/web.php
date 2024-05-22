a410c5ed869c90baf82d34a22270aa5edad3807d
ilyas mohetna <ilyas.mohetna@hotmail.com>
2024-05-22 23:03:59 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,6 @@ use App\Http\Controllers\ImportController;
 |
 */
 
-$proxy_url    = getenv('PROXY_URL');
-$proxy_schema = getenv('PROXY_SCHEMA');
-
-if (!empty($proxy_url)) {
-   URL::forceRootUrl($proxy_url);
-}
-
-if (!empty($proxy_schema)) {
-   URL::forceSchema($proxy_schema);
-}
-
 Route::get('/documentation/db', function(){
     return view('documentation');
 })->name('documentation');
