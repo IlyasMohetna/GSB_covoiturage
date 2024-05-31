c6d12c79018b1022e8f4b52daf5816e57901a21f
IlyasMohetna <ilyas.mohetna@hotmail.com>
2024-05-31 09:20:25 +0200

    test the ci cd pipeline

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>GSB - Intranet</title>
+		<title>Login - GSB - Intranet</title>
 		<script src="https://cdn.tailwindcss.com"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
